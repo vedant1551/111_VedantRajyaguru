20bc3c37a3f17dd2edff2fe65cf28dd352e83d35
Vedant Rajyaguru <vedantrajyaguru.333@gmail.com>
2021-08-19 23:31:47 +0530

    lab1_uploaded

--- index.py
+++ index.py
@@ -1,2 +1,26 @@
-import numpy  
-#demo lab
+import nltk # Python library for NLP
+from nltk.corpus import twitter_samples # sample Twitter dataset from NLTK
+import matplotlib.pyplot as plt # library for visualization
+import random # pseudo-random number generator
+#downloads sample twitter dataset.
+nltk.download('twitter_samples')
+
+# select the set of positive and negative tweets
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+
+print('Number of positive tweets: ', len(all_positive_tweets))
+print('Number of negative tweets: ', len(all_negative_tweets))
+print('\nThe type of all_positive_tweets is: ', type(all_positive_tweets))
+print('The type of a tweet entry is: ', type(all_negative_tweets[0]))
+
+
+
+fig = plt.figure(figsize=(5, 5))
+labels = 'Positives', 'Negative'
+sizes = [len(all_positive_tweets), len(all_negative_tweets)]
+plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+shadow=True, startangle=90)
+plt.axis('equal')
+plt.show()
+
